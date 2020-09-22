7e63e0511563ca94240afa14d169620934c339f2
yabramuvdi <yabran.muvdi@gmail.com>
2020-09-22 11:11:41 +0200

    update

--- data_generation.py
+++ data_generation.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import gaussian_kde
-import seaborn as sns
 
 # parameters
 n = 10000
@@ -11,9 +8,6 @@ income = np.random.normal(loc=800, scale=200, size=n)
 weights = np.random.randint(1,10000,size=n)
 sector = np.random.randint(1,7,size=n)
 print(sum(weights))
-
-# plot
-# sns.kdeplot(income, weights=weights)
 
 # save data
 df = pd.DataFrame({'income': income, 'weights': weights, 'sector': sector})
--- main.sh
+++ main.sh
@@ -1,4 +1,7 @@
+sudo apt update
+sudo apt install python3-pip
+
 python3 -m pip install -U pip setuptools wheel
 python3 -m pip install -r requirements.txt
-sh data_generation.py
-sh app.py
+python3 data_generation.py
+python3 app.py
